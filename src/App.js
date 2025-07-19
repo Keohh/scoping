ce18578cf051498e5d8428d390a74cac3af0a7c2
Keo <102829519+Keohh@users.noreply.github.com>
2025-07-19 16:05:51 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://r2.fivemanage.com/image/VAcimryiinQG.png"
+        alt="Background"
+        className="fullscreen-image"
+      />
+    </div>
+  );
+}
